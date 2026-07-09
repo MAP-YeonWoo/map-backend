a1b71f2eebf6859557e189f0cdb5b6526d10056e
yeonwoo0317 <yeonwoo070317@gmail.com>
2026-07-09 17:16:29 +0900

    feat: Movie 클래스 생성

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # map-backend
+### YeonWoo's Project ✫彡
+|MAP Project **YAHO**|
---
+++ practice/Movie.java
@@ -0,0 +1,11 @@
+public class Movie {
+    private String title;
+    private String genre;
+    private int releaseYear;
+
+    public Movie(String title, String genre, int releaseYear) {
+        this.title = title;
+        this.genre = genre;
+        this.releaseYear = releaseYear;
+    }
+}
